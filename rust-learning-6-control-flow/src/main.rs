c3bfcebb4896e9b1a1b2f6cfa42b6e8f19e20654
Vignesh <vigneshkwork7@gmail.com>
2026-04-19 14:32:29 +0530

    In Control flow, completed if, if-else, if-else-if expressions

---
+++ rust-learning-6-control-flow/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    // println!("If expression");
+
+    // let num = 7;
+
+    // // if statement
+    // if num != 10 {
+    //     println!("num is less than 10");
+    // }
+
+    // // if else statement
+    // if num < 10 {
+    //     println!("num is less than 10");
+    // } else {
+    //     println!("num is less than 10");
+    // }
+
+    // // passing non bool in condition example that will throw error
+    // // if num {
+    // //     println!("num is less than 10");
+    // // }
+
+    // if num % 2 == 0 {
+    //     println!("{} - number is even", num)
+    // } else if num % 2 != 0 {
+    //     println!("{} - number is odd", num)
+    // } else {
+    //     println!("{} - It is neitheir even or odd", num)
+    // }
+
+    // let condition = true;
+    // let num = if condition { 5 } else { 6 };
+    // println!("output: {num}");
+
+    // let condition = true;
+    // let str = if condition { "vic" } else { 6 }; // This will throw error because both if and else expression arm must return same type
+    // println!("output: {str}");
+}
